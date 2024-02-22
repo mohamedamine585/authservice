4ccf747ff0f81f42a5e37473d163a0e97b1e5bf9
Mohamed amine Tlili <foxweb585@gmail.com.com>
2024-02-22 17:07:21 +0100

    +

--- routes/entry.dart
+++ routes/entry.dart
@@ -1,10 +1,8 @@
 import 'package:shelf_router/shelf_router.dart';
 
-import 'handlers/roothandler.dart';
 import 'handlers/signinhandler.dart';
 import 'handlers/signuphandler.dart';
 
 // Configure routes.
 Router router = Router()
   ..post('/signin', signinHandler)
-  ..post('/signup', signupHandler);
