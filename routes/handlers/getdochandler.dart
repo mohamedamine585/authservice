833ead06c6ac2965763c008fa25b53b72a0f4c93
Mohamed amine Tlili <foxweb585@gmail.com.com>
2024-02-25 17:44:12 +0100

    ++

--- routes/handlers/getdochandler.dart
+++ routes/handlers/getdochandler.dart
@@ -4,9 +4,25 @@ import 'package:shelf/shelf.dart';
 import '../../repositories/playerrepository.dart';
 
 Future<Response> getdocHandler(Request req) async {
-  try {} catch (e) {
+  try {
+    final doc = await PlayerRepository.getdoc(id: req.context["_id"] as String);
+    if (doc?.isNotEmpty ?? false) {
+      return Response.ok(json.encode({
+        "playername": doc!["playername"],
+        "email": doc["email"],
+        "playedgames": doc["playedgames"],
+        "wongames": doc["wongames"],
+        "rating": doc["rating"],
+        "country": doc["country"]
+      }));
+    } else {
+      return Response.ok(
+          json.encode({"message": "No doc found for this user"}));
+    }
+  } catch (e) {
     print(e);
   }
+
   // refine later
   return Response(404);
 }
