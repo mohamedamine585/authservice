e774c7974eeb37452597f9323cf9c87bd2192b89
Mohamed amine Tlili <foxweb585@gmail.com.com>
2024-02-26 17:29:35 +0100

    test+

--- test/server_test.dart
+++ test/server_test.dart
@@ -1,54 +1,38 @@
 import 'dart:convert';
 import 'dart:io';
-import 'package:http/http.dart';
 import 'package:test/test.dart';
-import 'package:http/http.dart';
-import 'package:mockito/mockito.dart';
-
-class MockClient extends Mock implements Client {}
+import 'package:http/http.dart' as http;
 
 void main() async {
-  Process? process;
-  final String baseUrl = 'http://0.0.0.0:8080';
+  final String baseUrl = 'http://127.0.0.1:8080';
   final String email =
       'test${DateTime.now().microsecondsSinceEpoch}@example.com';
   final String password = "password123";
-  setUpAll(() async {
-    final mockClient = MockClient();
-    when(mockClient.head(Uri.parse(baseUrl))).thenAnswer(
-        (_) => Future.value(Response('{"message": "success"}', 404)));
-
-    // Start the server process
-    process = await Process.start("dart", ["bin/server.dart"]);
-  });
 
   group("Authentication testing", () {
     test('Signup Test', () async {
       final Uri signupUrl = Uri.parse('$baseUrl/signup');
       final Map<String, String> body = {'email': email, 'password': password};
+      final http.Response response =
+          await http.post(signupUrl, body: json.encode(body));
 
-      final Response response = await post(signupUrl, body: json.encode(body));
-
+      expect(response.statusCode, 200);
       expect(response.headers['content-type'], 'application/json');
       expect(response.headers['authorization'], isNotNull);
       expect(response.headers['authorization']!.startsWith('Bearer '), isTrue);
     });
-
     test('Signin Test', () async {
       final Uri signupUrl = Uri.parse('$baseUrl/signin');
       final Map<String, String> body = {'email': email, 'password': password};
+      final http.Response response =
+          await http.post(signupUrl, body: json.encode(body));
 
-      final Response response = await post(signupUrl, body: json.encode(body));
-
+      expect(response.statusCode, 200);
       expect(response.headers['content-type'], 'application/json');
       expect(response.headers['authorization'], isNotNull);
       expect(response.headers['authorization']!.startsWith('Bearer '), isTrue);
     });
-  });
 
-  // Terminate the server process
-  tearDownAll(() async {
-    process?.kill(ProcessSignal.sigkill);
-    print('Server process terminated.');
+    // Add other tests as needed
   });
 }
